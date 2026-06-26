c1a000c4e4798dd242754ee3076cc7e900164081
shivapanwar089-blip <shivapanwar089@gmail.com>
2026-06-26 18:53:15 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -39,6 +39,90 @@ loadTeams();
 
 function renderTable(){
 
+    // Clear old table
     teamTable.innerHTML = "";
+
+    // Convert Firebase object into an array
+    const sortedTeams = Object.entries(teams);
+
+    // Sort by kills (highest first)
+    sortedTeams.sort((a,b)=>{
+
+        return b[1].kills - a[1].kills;
+
+    });
+
+    // Create one row for every team
+    sortedTeams.forEach(([id,team],index)=>{
+
+        teamTable.innerHTML += `
+
+<tr>
+
+<td>${index+1}</td>
+
+<td>
+
+<img
+class="teamLogo"
+src="${team.logo}"
+width="45"
+height="45">
+
+</td>
+
+<td>
+
+${team.name}
+
+</td>
+
+<td>
+
+<button onclick="changeKills('${id}',-1)">-</button>
+
+<b>${team.kills}</b>
+
+<button onclick="changeKills('${id}',1)">+</button>
+
+</td>
+
+<td>
+
+<div>
+
+<b>Alive : ${team.alive}</b>
+
+</div>
+
+<br>
+
+<button onclick="setAlive('${id}',4)">4</button>
+
+<button onclick="setAlive('${id}',3)">3</button>
+
+<button onclick="setAlive('${id}',2)">2</button>
+
+<button onclick="setAlive('${id}',1)">1</button>
+
+<button onclick="setAlive('${id}',0)">0</button>
+
+</td>
+
+<td>
+
+<button onclick="deleteTeam('${id}')">
+
+Delete
+
+</button>
+
+</td>
+
+</tr>
+
+`;
+
+    });
 
 }
