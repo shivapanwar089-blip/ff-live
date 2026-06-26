9f05cdc7c3fce324a451d3d87cf189262892df50
shivapanwar089-blip <shivapanwar089@gmail.com>
2026-06-26 18:46:46 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,14 @@
+import { db } from "../firebase.js";
 
+import {
+    ref,
+    onValue,
+    update,
+    push,
+    remove,
+    get
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const teamTable = document.getElementById("teamTable");
+
+let teams = {};
