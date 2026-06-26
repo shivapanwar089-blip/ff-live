b94cd2d667f8f66ef52edace1697a89433c98964
shivapanwar089-blip <shivapanwar089@gmail.com>
2026-06-26 18:51:05 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -12,3 +12,33 @@ import {
 const teamTable = document.getElementById("teamTable");
 
 let teams = {};
+
+// ----------------------
+// LOAD TEAMS FROM FIREBASE
+// ----------------------
+
+function loadTeams() {
+
+    const teamsRef = ref(db, "teams");
+
+    onValue(teamsRef, (snapshot) => {
+
+        teams = snapshot.val() || {};
+
+        renderTable();
+
+    });
+
+}
+
+loadTeams();
+
+// ----------------------
+// DRAW TABLE
+// ----------------------
+
+function renderTable(){
+
+    teamTable.innerHTML = "";
+
+}
